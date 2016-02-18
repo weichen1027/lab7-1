c025c0faee6bfeca123232717de81bd653d4f7f4
Weichen Wang <wew129@eng.ucsd.edu>
2016-02-18 05:00:22 +0000

    like timing

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var startTime;
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -10,11 +10,15 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	startTime = new Date().getTime();
 	$(".project .likeCtr").click(projectLike);
 	$(".project .likeCtrGrid").click(projectLike);
 }
 
 function projectLike(e) { 
-	ga("send", "event", 'like', 'click');
+	var endTime = new Date().getTime();
+	sga("send", "event", 'like', 'click');
 
+	var timeSpent = endTime - startTime;
+	_gaq.push(['_trackTiming', 'Click_Like', 'Time_to_Like', timeSpent]);
 }
